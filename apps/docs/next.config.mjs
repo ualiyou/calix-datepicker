58e29da03f6065e7790f466804880332d4960b0e
Ali Yousefi <ayousefi@outlook.com>
2026-07-27 14:42:45 +0330

    feat(docs): deploy static docs to pages

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,10 +1,14 @@
 import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
 
 /** @type {import('next').NextConfig} */
 const config = {
+  basePath,
+  output: "export",
   reactStrictMode: true,
+  trailingSlash: true,
 };
 
 export default withMDX(config);
